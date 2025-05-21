ad974be129f5ec365cdcafda0099db562a315acb
Eduarda <eduardaveloso@outlook.com.br>
2025-05-21 15:30:19 -0300

    config atualizada no db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,19 @@
-
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'CURRICULOEX',
-  password: '123456db',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
-
 
 pool.connect((err, client, release) => {
   if (err) {
     console.error('Erro ao conectar ao PostgreSQL:', err.stack);
   } else {
     console.log('Conexão com PostgreSQL estabelecida!');
-    release(); 
+    release();
   }
 });
 
