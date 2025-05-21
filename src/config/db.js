5e27b1140dd1f0a33f3bb14e6ec5159871715484
Eduarda <eduardaveloso@outlook.com.br>
2025-05-21 15:17:02 -0300

    instalação do  módulo serverless-http

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,4 @@
-// src/config/db.js
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -6,16 +6,16 @@ const pool = new Pool({
   host: '127.0.0.1',
   database: 'CURRICULOEX',
   password: '123456db',
-  port: 5432, // Porta padrão do PostgreSQL
+  port: 5432,
 });
 
-// Teste a conexão ao iniciar
+
 pool.connect((err, client, release) => {
   if (err) {
     console.error('Erro ao conectar ao PostgreSQL:', err.stack);
   } else {
     console.log('Conexão com PostgreSQL estabelecida!');
-    release(); // Libera o cliente de volta para o pool
+    release(); 
   }
 });
 
