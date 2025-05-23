47633f01d5ec90bb91f085cef23c63bf933a949d
Eduarda <eduardaveloso@outlook.com.br>
2025-05-23 12:05:03 -0300

    fix: Garante que o servidor Express so inicializa após a conexão com o banco de dado

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
-const app = require('../src/app');
+const app = require('./app');
+const pool = require('./config/db'); 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+
+
+pool.query('SELECT 1')
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso no Vercel!');
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro fatal ao conectar ao banco de dados no Vercel, encerrando app:', err.message);
+    process.exit(1); 
+  });
